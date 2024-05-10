2805aa63099859ea04edf562d48f6e30fc3616a3
Ankit Mahotla <ankitmahotla45@gmail.com>
2024-05-10 11:42:23 +0530

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Ankit Mahotla on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
